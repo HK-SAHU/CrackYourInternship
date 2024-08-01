bda73a0f9ba9d0adf74fe1c52baf23a3033446ab
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-08-01 10:04:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.java
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class LinkedList
+{
+    
+ 
+
+    /* Function to print linked list */
+   public static void print(Node root)
+    {
+        Node temp = root;
+        while(temp!=null)
+        {
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+
+
+    /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+
+        /* Constructed Linked List is 1.2.3.4.5.6.
+         7.8.8.9.null */
+        int value;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0 && sc.hasNextInt() )
+        {
+            int n = sc.nextInt();
+            
+            //int n=Integer.parseInt(br.readLine());
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+
+            for (int i = 1; i < n && sc.hasNextInt() ; i++)
+            {
+                int a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+
+            Node result = new Solution().compute(head);
+            print(result);
+            System.out.println();
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node {
+   int data;
+   Node next;
+
+  Node(int data) {
+      this.data = data;
+  }
+}
+*/
+class Solution
+{
+    Node compute(Node head)
+    {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        Node reversed = reverseList(head);
+        Node newList = new Node(reversed.data); // Start with the first node
+        Node newTemp = newList; // Pointer to construct the new list
+        Node temp = reversed.next; // Pointer to iterate over the reversed list
+        int maxSoFar = reversed.data; // The first node in the reversed list is the max initially
+
+        while (temp != null) {
+            if (temp.data >= maxSoFar) {
+                maxSoFar = temp.data;
+                newTemp.next = new Node(temp.data); // Add node to new list
+                newTemp = newTemp.next;
+            }
+            temp = temp.next;
+        }
+
+        return reverseList(newList); // Reverse the new list to get the final output
+    }
+    
+    private Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
+  
