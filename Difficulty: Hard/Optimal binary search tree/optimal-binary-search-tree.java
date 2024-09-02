1efee48a7af6a39266e83ecac619f47123721ae7
Harsh Kumar Sahu <135067768+HK-SAHU@users.noreply.github.com>
2024-09-02 22:25:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Optimal binary search tree/optimal-binary-search-tree.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int keys[]= new int[n];
+            for(int i = 0; i < n; i++)
+                keys[i] = Integer.parseInt(input_line[i]);
+            String input_line1[] = read.readLine().trim().split("\\s+");
+            int freq[]= new int[n];
+            for(int i = 0; i < n; i++)
+                freq[i] = Integer.parseInt(input_line1[i]);
+            Solution ob = new Solution();
+            System.out.println(ob.optimalSearchTree(keys, freq, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int optimalSearchTree(int keys[], int freq[], int n) {
+        int[][] cost = new int[n][n];
+
+        // Initialize cost for single keys
+        for (int i = 0; i < n; i++) {
+            cost[i][i] = freq[i];
+        }
+
+        // L is chain length
+        for (int L = 2; L <= n; L++) {
+            for (int i = 0; i <= n - L; i++) {
+                int j = i + L - 1;
+                cost[i][j] = Integer.MAX_VALUE;
+
+                // Try making all keys in interval keys[i..j] as root
+                for (int r = i; r <= j; r++) {
+                    int c = ((r > i) ? cost[i][r - 1] : 0) +
+                            ((r < j) ? cost[r + 1][j] : 0) +
+                            sum(freq, i, j);
+                    if (c < cost[i][j]) {
+                        cost[i][j] = c;
+                    }
+                }
+            }
+        }
+        return cost[0][n - 1];
+    }
+
+    static int sum(int freq[], int i, int j) {
+        int s = 0;
+        for (int k = i; k <= j; k++) {
+            s += freq[k];
+        }
+        return s;
+    }
+}
